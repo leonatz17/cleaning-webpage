133a44bc9f0f6e73c1210fb9e062aa6e0f511b1a
leonatz17 <natzyleon@gmail.com>
2026-05-21 10:27:26 +0800

    a

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -16,14 +16,7 @@
                 AN ESTIMATE</button>
         </div>
     </div>
-    <div class=" mt-10  flex justify-center px-10">
-        <div class="lg:w-100 md:w-80 sm:50 border text-center font-serif border-hidden">
-            <h1 class="text-2xl font-bold py-3">What do we do?</h1>
-            <p class="text-[15px]">Experience unparalleled cleaning services from our Clean Sparks Agents. Whether it’s
-                your residence, commercial
-                space, or a post-construction site, rest assured that you’ll receive top-quality service.</p>
-        </div>
-    </div>
+   
 
     <div class="flex justify-center mt-10">
         <div class="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1  gap-5">
