4f4d8b40740bce6c272dbe16c8cd590bb1ce8156
leonatz17 <natzyleon@gmail.com>
2026-05-21 09:59:10 +0800

    mt t&e

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,7 +3,7 @@
 </script>
 <template>
     
-    <div class="relative lg:mt-10 md:mt-10 sm:mt-60">
+    <div class="relative mt-30">
         <div>
             <img class="h-60 w-full shrink-0" src="../assets/images/cleanspace.jpg" alt="">
         </div>
