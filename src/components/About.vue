7a3fa221181a3ffa82c92a8950a9db9f44db0ac6
leonatz17 <natzyleon@gmail.com>
2026-05-21 10:26:58 +0800

    make about

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,5 +1,14 @@
 <script>
 </script>
 <template>
-    <h1>About</h1>
+    <div>
+        <div class=" mt-10  flex justify-center px-10">
+        <div class="lg:w-100 md:w-80 sm:50 border text-center font-serif border-hidden">
+            <h1 class="text-2xl font-bold py-3">What do we do?</h1>
+            <p class="text-[15px]">Experience unparalleled cleaning services from our Clean Sparks Agents. Whether it’s
+                your residence, commercial
+                space, or a post-construction site, rest assured that you’ll receive top-quality service.</p>
+        </div>
+    </div>
+    </div>
 </template>
